595e43eacdf2b4f702b19018dbf7b0a2f6e0ca9c
wf <583221850@qq.com>
2019-08-21 17:16:57 +0800

    简单了理解了cmake

---
+++ SELF_STUDY/cmake_practice/t1/build/CMakeFiles/hello2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello2.dir/main.cpp.o"
+  "hello2.pdb"
+  "hello2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
